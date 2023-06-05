db68676c26d592af1b11b87087d49be1bb7bc44f
wpxmunna <wpxmunna@gmail.com>
2023-06-05 17:03:40 +0600

    without responsive and without contact us and footer section

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".menu");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("burger-active");
-  menu.classList.toggle("menu-active");
-});
+document.querySelector('#menu').addEventListener('click',()=>{
+    document.querySelector('nav ul').classList.toggle('showmenu')
+})
